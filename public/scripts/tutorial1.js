7a42ac4c3e6288d14da5726aac8df9dd1a6a355d
Craig Fisk <craigfisk@picocosmos.net>
2016-08-14 16:39:26 -0700

    Using tutorial version with react 15.3 from a CDN, ran through react-tutorial "tutorial<n>" versions, up to where fetch of data from a RESTful api is introduced.

---
+++ public/scripts/tutorial1.js
@@ -0,0 +1,17 @@
+/* scripts/tutorial1.js */
+
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('Fred')
+);
